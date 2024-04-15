5b50ef16602d28109f7772403706b32b3e477486
anotherly <anotherly91@gmail.com>
2024-04-15 16:18:08 +0900

    파일경로 변경

--- CloudAdmin/src/main/java/kr/co/hivesys/comm/file/FileUploadSave.java
+++ CloudAdmin/src/main/java/kr/co/hivesys/comm/file/FileUploadSave.java
@@ -1,6 +1,7 @@
 package kr.co.hivesys.comm.file;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -26,10 +27,6 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import kr.co.hivesys.comm.file.service.FileService;
 import kr.co.hivesys.comm.file.vo.FileVo;
 
-//빌어먹을 이걸 의존성 주입으로 해야하는데..
-//객체 생성으로 딴데서 부르니까
-//밑에 서비스를 소환을 못하지 ㅠㅠ
-//왜 구멍난 젠가 같은걸까
 @Component("fileLogic")
 public class FileUploadSave implements ApplicationContextAware {
 
@@ -42,7 +39,7 @@ public class FileUploadSave implements ApplicationContextAware {
 	//윈도우용
 	//private String firstPath="C:\\";
 	//리눅스용
-	private String firstPath="/usr/local/tomcat/share_data";
+	private String firstPath="/usr/local/tomcat/share_data/";
 	/**
 	 * 업로드할 파일의 상세 정보 얻어옴
 	 * 
@@ -57,42 +54,45 @@ public class FileUploadSave implements ApplicationContextAware {
 	// 여러 개의 파일 업로드 + 디비저장
 	// (insert or update)
 
-	/*
-	 * 초기에는 파일 경로를 읽어오고 쿼리를 타는것을 최소화 하는 방안으로 할려했으나 
-	 * 마리아디비가 merge를 지원하지 않고 dupicate로 하는 방식은 
-	 * 오라클만큼 매끄럽지 않고 다중 파일의 경우 이것저것 변수가 많아지므로 
-	 * 초기에 select 쿼리를 짜서 기존 파일 판별
-	 * 파일id도 다중파일일 경우 여러개 받아와야하는데 난잡해짐...
-	 */
-
 	public int fileUploadMultiple(List<MultipartFile> files, FileVo fvo) {
 		List<FileVo> fileList = new ArrayList<>();
 		List<FileVo> exList = new ArrayList<>();
 		int i = 0;
 		try {
+			
+			logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ firstPath ■■■■■■■■■■■■■■■■■■■■■■■■■ : "+firstPath);
+			logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ fvo.getFILE_DIR() ■■■■■■■■■■■■■■■■■■■■■■■■■ : "+fvo.getFILE_DIR());
+			
 			// 1. 파일 저장 경로 설정 : 실제 서비스되는 위치(프로젝트 외부에 저장)
 			String inputPath = firstPath + fvo.getFILE_DIR();
+			logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 1. inputPath ■■■■■■■■■■■■■■■■■■■■■■■■■ : "+inputPath);
 			// 1.1 기존 파일 존재여부 판단
 			exList = fileService.selectFileList(fvo);
 			// 여러 개의 원본 파일을 저장할 리스트 생성
 			for (MultipartFile mfile : files) {
 				// 2. 원본 파일 이름 알아오기
 				String originalFileName = mfile.getOriginalFilename();
+				logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 2. 원본 파일 이름 알아오기 ■■■■■■■■■■■■■■■■■■■■■■■■■ : "+originalFileName);
 				// 4. 파일 이름 중복되지 않게 이름 변경(서버에 저장할 이름) UUID 사용
 				// 5. 파일 생성
 				File file = new File(inputPath + originalFileName);// 경로+파일
+				logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 3. 파일 생성 ■■■■■■■■■■■■■■■■■■■■■■■■■ : "+file);
 
 				// 아래로직은 반복문을 여러번 탈필요 없음 최초에만
 				if (i == 0) {
 					if (exList.size() == 0) {// 신규 생성
+						logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 파일경로 신규 생성 ■■■■■■■■■■■■■■■■■■■■■■■■■");
 						file.getParentFile().mkdirs();// 파일 상부의 모든 존재하지 않는 경로 생성
+						logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 파일경로 신규 생성 완료 ■■■■■■■■■■■■■■■■■■■■■■■■■");
 					} else { // 기존 것 날리고 새로
 						fileDelete(fvo);
+						logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 파일경로 내 파일 기존것 삭제 ■■■■■■■■■■■■■■■■■■■■■■■■■");
 					}
 				}
 				// 6. 서버내 파일 경로로 전송
 				// 한번 트랜스퍼 된건 또다시 트랜스퍼 되지 않음 거지같은...
 				mfile.transferTo(file);
+				logger.debug("■■■■■■■■■■■■■■■■■■■■■■■■■ 파일 경로로 전송 완료 ■■■■■■■■■■■■■■■■■■■■■■■■■");
 
 				// 7.리턴값 적재
 				FileVo dbvo = new FileVo();
